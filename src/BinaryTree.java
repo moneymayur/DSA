be2df7e19d7e4ea48ed09a8958165047725ec03c
Mayur Mathur <moneymayur@users.noreply.github.com>
2017-04-05 23:09:27 +0530

    Update BinaryTree.java

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -18,6 +18,39 @@ public class BinaryTree {
         root = null;
     }
     
+    int height(Node n){
+    	int h = 0;
+    	if(n == null)
+    		return 0;
+    	else{
+    		int lh = height(n.Left);
+    		int rh = height(n.Right);
+    		if(lh>rh)
+    			h = lh + 1;
+    		else
+    			h = rh + 1;	
+    	}
+    	return h;
+    	
+    }
+    
+    void printLevelOrder(){
+    	int h = height(root);
+    	for(int i=0;i<=h;i++){
+    	printGivenOrder(root,i);}
+    }
+    
+    void printGivenOrder(Node n, int i){
+    	if(root == null)
+    		return;
+    	else if( i == 1)
+    	   System.out.println(n.data);		
+    	else if (i>1)
+    		{printGivenOrder(n.Left, i-1);
+			printGivenOrder(n.Right, i-1);}	
+    
+    }
+    
     void printPostOrder(){
     	printPostOrder(root);
     }
@@ -67,6 +100,7 @@ public class BinaryTree {
 	t.printInorder();
 	t.printPostOrder();
 	t.printPreOrder();
+	t.printLevelOrder();
 	//printNode(a);
 }
 
