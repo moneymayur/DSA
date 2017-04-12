3b2dbf3404de29dfac43864d31a458ad95ec1e1e
Mayur Mathur <moneymayur@users.noreply.github.com>
2017-04-12 15:37:54 +0530

    Update BinarySearch.java

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,3 +1,8 @@
+- Given a sorted array of N distinct elements. Find a key in the array using least number of comparisons. (Do you think binary search is optimal to search a key in sorted array?)
+- Given an array of N distinct integers, find floor/Ceil value of input ‘key’. Say, A = {-1, 2, 3, 5, 6, 8, 9, 10} and key = 7, we should return 6 as outcome.
+- Given a sorted array with possible duplicate elements. Find number of occurrences of input ‘key’ in log N time.
+- Given a sorted array of distinct elements, and the array is rotated at an unknown position. Find minimum element in the array.
+
 
 public class BinarySearch {
 	int count = 10;
